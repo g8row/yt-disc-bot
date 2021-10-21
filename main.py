3a8c331221bd5be6eba0fcf85cf8618abfba6395
gurow-wsl <aliogu23@gmail.com>
2021-10-21 16:32:50 +0300

    youtube-dlp mp3 downloading

--- main.py
+++ main.py
@@ -3,19 +3,41 @@ import re
 import json
 import requests
 import simplejson
-
+import youtube_dl
+import yt_dlp
 
 html = urllib.request.urlopen("https://www.youtube.com/results?search_query=yeat")
 string = html.read().decode()
 index = 0
-for video in re.findall(r"watch\?v=(\S{11})", string):   
-    params = {"format": "json", "url": "https://www.youtube.com/watch?v=%s" % video}
-    url = "https://www.youtube.com/oembed"
-    query_string = urllib.parse.urlencode(params)
-    url = url + "?" + query_string
-    with urllib.request.urlopen(url) as response:
-        response_text = response.read()
-        data = json.loads(response_text.decode())
-        print(data['title'])
+results = re.findall(r"watch\?v=(\S{11})", string);
 
 
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+ydl =  yt_dlp.YoutubeDL(ydl_opts)
+
+for index, video in enumerate(results):
+    if index != 5:
+        with ydl:
+            result = ydl.extract_info(
+            'http://www.youtube.com/watch?v=' + video,
+            download=False
+            )
+        if 'entries' in result:
+            # Can be a playlist or a list of videos
+            title = result['entries'][0]
+        else:
+            # Just a video
+            title = result['title']
+        print(title)
+    else:
+        break
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['http://www.youtube.com/watch?v=' + results[0]])
